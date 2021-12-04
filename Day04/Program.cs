53f6d2520dbc272261f5a2df0eab679594371211
Carlos <clockwerkz@gmail.com>
2021-12-04 00:15:24 -0800

    starting on day 4

---
+++ Day04/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day04
+{
+    class Board
+    {
+        List<List<string>> board = new List<List<string>>();
+        Board(List<string> _board)
+        {
+            _board.ForEach(line => board.Add(new List<string>(line.Split(' '))));
+        }
+
+        public void PrintBoard()
+        {
+            board.ForEach(line => line.ForEach(entry => Console.Write(entry + " ")));
+        }
+
+        public bool CheckBoard(string number)
+        {
+
+            return true;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            string currentFile = projectDirectory + "\\Sample.txt";
+            List<string> lines = new List<String>(File.ReadAllLines(currentFile));
+            string instructions = lines[0];
+            lines.RemoveAt(0);
+            lines.ForEach(line => Console.WriteLine(line));
+        }
+    }
+}
