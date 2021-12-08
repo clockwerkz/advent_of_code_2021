cc3e8963ee9346cf095ddc21e05acc83b1810742
Carlos <clockwerkz@gmail.com>
2021-12-08 10:48:16 -0800

    part 1 complete

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Day08
 {
@@ -6,7 +7,33 @@ namespace Day08
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            string currentFile = projectDirectory + "\\Day8Input.txt";
+            string[] lines = File.ReadAllLines(currentFile);
+            Console.WriteLine("---DAY 8: PART 1---");
+            Console.Write("Day 8 Part 1 Input Answer: ");
+            CalculatePartOne(lines);
+        }
+
+        static void CalculatePartOne(string[] lines)
+        {
+            if (lines.Length == 0) return;
+            int digitCount = 0;
+            //1 = 2 segments,4  = 4 segments,7 = 3 segments,8 = 7 segments
+            foreach (string line in lines)
+            {
+                string panelLine = line.Split(" | ")[1];
+                string[] display = panelLine.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                foreach (string digit in display)
+                {
+                    if (digit.Length == 7  || digit.Length == 2 || digit.Length == 3 || digit.Length == 4)
+                    {
+                        digitCount++;
+                    }
+                }
+            }
+            Console.WriteLine(digitCount);
         }
     }
 }
